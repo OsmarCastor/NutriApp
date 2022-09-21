922e91790e88a915e0f31eb5ad0f77ecc44c8596
Osmar Castillo <te190119005@tequila.tecmm.edu.mx>
2022-09-21 06:50:59 -0500

    Seguimiento

---
+++ app/src/main/java/castor/no/c/programar/nutriapp/agregarPaciente.kt
@@ -0,0 +1,11 @@
+package castor.no.c.programar.nutriapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class agregarPaciente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_paciente)
+    }
+}
---
+++ app/src/main/java/castor/no/c/programar/nutriapp/mostrarPacientes.kt
@@ -0,0 +1,11 @@
+package castor.no.c.programar.nutriapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mostrarPacientes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar_pacientes)
+    }
+}

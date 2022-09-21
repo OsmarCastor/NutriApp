02aafdbb42ba22c1a47bcd9f32e4953933c103b8
Osmar Castillo <te190119005@tequila.tecmm.edu.mx>
2022-09-21 08:55:20 -0500

    Hago commit

--- app/src/main/java/castor/no/c/programar/nutriapp/agregarPaciente.kt
+++ app/src/main/java/castor/no/c/programar/nutriapp/agregarPaciente.kt
@@ -2,10 +2,24 @@ package castor.no.c.programar.nutriapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class agregarPaciente : AppCompatActivity() {
+    lateinit var txtNombre:EditText
+    lateinit var txtEdad:EditText
+    lateinit var txtTelefono:EditText
+    lateinit var txtFecha:EditText
+    lateinit var txtPeso:EditText
+    lateinit var txtEstatura:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_paciente)
+        txtNombre = findViewById(R.id.txtNombre)
+        txtEdad = findViewById(R.id.txtEdad)
+        txtTelefono = findViewById(R.id.txtTelefono)
+        txtFecha = findViewById(R.id.txtNacimiento)
+        txtPeso = findViewById(R.id.txtPeso)
+        txtEstatura = findViewById(R.id.txtEstatura)
     }
+
 }

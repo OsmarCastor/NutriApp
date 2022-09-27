b822955dba6c8b03d4327dd9e3c725e02c1ff6da
Osmar Castillo <te190119005@tequila.tecmm.edu.mx>
2022-09-27 07:48:41 -0500

    Hago commit

--- app/src/main/java/castor/no/c/programar/nutriapp/formularioPacientes.kt
+++ app/src/main/java/castor/no/c/programar/nutriapp/formularioPacientes.kt
@@ -7,5 +7,6 @@ class formularioPacientes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_pacientes)
+        supportActionBar?.hide()
     }
 }

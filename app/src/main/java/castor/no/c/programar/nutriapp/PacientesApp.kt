d5471ef697d656003741a2631c15322495858f0b
Osmar Castillo <te190119005@tequila.tecmm.edu.mx>
2022-09-29 08:53:59 -0500

    Hago commit

--- app/src/main/java/castor/no/c/programar/nutriapp/PacientesApp.kt
+++ app/src/main/java/castor/no/c/programar/nutriapp/PacientesApp.kt
@@ -4,6 +4,10 @@ import android.app.Application
 import androidx.room.Room
 
 class PacientesApp : Application() {
-    val room = Room.databaseBuilder(this, AppDatabase::class.java, "paciente").build()
+    lateinit var room: AppDatabase
 
+    override fun onCreate() {
+        super.onCreate()
+        room =  Room.databaseBuilder(this, AppDatabase::class.java, "paciente").build()
+    }
 }
--- app/src/main/java/castor/no/c/programar/nutriapp/mostrarPaciente.kt
+++ app/src/main/java/castor/no/c/programar/nutriapp/mostrarPaciente.kt
@@ -3,27 +3,28 @@ package castor.no.c.programar.nutriapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.log
 
 class mostrarPaciente : AppCompatActivity() {
     lateinit var recycler: RecyclerView
+    //lateinit var db :PacientesApp
     companion object{
-        //val pacientes = ArrayList<paciente>()
+        //val dao = PacientesApp()
+        //val lista= dao.room.pacienteDao().getAll()
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_paciente)
         supportActionBar?.hide()
         recycler = findViewById(R.id.rv_persona)
-
+        val db = this.application as PacientesApp
+        Log.e("Algo","asasas")
     }
     fun abrirAregar(v: View){
         val intent = Intent(this, agregarPaciente::class.java)
-        startActivity(intent)
-    }
-    fun abrirForm(v: View){
-        val intent = Intent(this, formularioPacientes::class.java)
         startActivity(intent)
     }
     override fun onPostResume() {
@@ -31,9 +32,8 @@ class mostrarPaciente : AppCompatActivity() {
         actualizarRecycler()
     }
     fun actualizarRecycler(){
-        val dao = PacientesApp()
-        val lista= dao.room.pacienteDao().getAll()
-        val adaptador = Adaptador(this, lista)
-        recycler.adapter = adaptador
+        //val lista = db.room.pacienteDao().getAll()
+        //val adaptador = Adaptador(this, lista)
+        //recycler.adapter = adaptador
     }
 }
